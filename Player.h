68dbaf068029c4c90b082f254b9cd33ebb42e403
Hodaya Sror <33117494+hodayasr@users.noreply.github.com>
2018-05-24 04:11:26 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -7,16 +7,19 @@ using Coordinate = BoardIndex;
 class Player{
 
 public:
-char myChar;
-    void setChar(char c)
-    {
-        myChar = c;
-    }
     
+char myChar;
+        
     char getChar() const
     {
         return myChar;
     }
+    
+    void setChar(char c)
+    {
+        myChar = c;
+    }
+
 
     virtual const std::string name() const = 0;
     virtual const Coordinate play(const Board& board) = 0;
