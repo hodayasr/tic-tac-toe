2e636c4d795f686b3bd0c288889c9076f87bb8d4
Hodaya Sror <33117494+hodayasr@users.noreply.github.com>
2018-06-05 10:31:32 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,203 +1,274 @@
 #include "Board.h"
 
 
+using namespace std;
 
-Board::Board(int value)
-{
-	this->n = value;
-	mat = new Node*[value];
-
-	for(int i = 0; i < n ;i++)
-	{
-		mat[i] = new Node[n];
-	}
-}
-
-Board::Board(const Board& other)
-{
-	/*if(size()>0){
-		this->~Board();
-	}*/
-	this->n = other.size();
-	mat = new Node*[n];
-
-	for(int i = 0; i < n ;i++)
-	{
-		mat[i] = new Node[n];
-	}
-	for(int i = 0; i < n ;i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			mat[i][j] = other.mat[i][j];
+		Board::Board(){
+			len = 0;
 		}
-	}	
-}
-
-int Board::size() const
-{
-	return this->n;
-}
-
-Board::~Board()
-{
-	for(int i = 0; i < this->n; i++){
-		delete[] mat[i];
-	}
-	delete[] mat;
-}
-
-Node& Board::operator[](list<int> index)
-{
-	int a = index.front();
-	int b = index.back();
-
-	BoardIndex bIndex(a,b);
-
-	if(bIndex.i < n && bIndex.i >= 0 && bIndex.j < n && bIndex.j >= 0)
-	{
-		return mat[bIndex.i][bIndex.j];
-	}
-
-	else
-	{
-		throw IllegalCoordinateException(bIndex);
-	}
-}
-
-Node& Board::operator[](const BoardIndex& coor) const
-{
-	int a = coor.i;
-	int b = coor.j;
-
-	BoardIndex bIndex(a,b);
-
-	if(bIndex.i < n && bIndex.i >= 0 && bIndex.j < n && bIndex.j >= 0)
-	{
-		return mat[bIndex.i][bIndex.j];
-	}
-
-	else
-	{
-		throw IllegalCoordinateException(bIndex);
-	}
-}
-Board& Board::operator=(char value)
-{
-	if(value != '.' && value != 'X' && value != 'O')
-	{
-		throw IllegalCharException(value);
-	}
-	else
-	{
-		for(int i = 0; i < this->n; i++){
-			for(int j = 0; j < this->n; j++){
-				this->mat[i][j].setSymbol(value);
-			}
-		}
-
-	}
-	return *this;
-}
-
-Board& Board::operator=(const Board& db)
-{
-	this->~Board();
-	
-	this->n = db.n;
-	mat = new Node*[n];
-
-	for(int i = 0; i < n ;i++)
-	{
-		mat[i] = new Node[n];
-	}
-
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			mat[i][j].setSymbol(db.mat[i][j].getSymbol());
-		}
-	}
-	return *this;
-}
-
-Board& Board::operator=(int value){
-
-	n = value;
-	mat = new Node*[n];
-
-	for(int i = 0; i < n ;i++)
-	{
-		mat[i] = new Node[n];
-	}
-	return *this;
-}
-
-struct RGB {
-	uint8_t red, green, blue;
-public:
-	RGB() {}
-	RGB(uint8_t red, uint8_t green, uint8_t blue): red(red), green(green), blue(blue) {}
-};
-
-string Board::draw(int val){
-	const int dimx = val;
-	const int dimy = val;
-	string filename="cpp"+to_string(n)+".ppm";
-	ofstream imageFile(filename, ios::out | ios::binary);
-	imageFile << "P6" << endl << dimx <<" " << dimy << endl << 255 << endl;
-	RGB image[dimx*dimy];
-	int charsize=val/n;
-	for(int i = 0; i < n; i++)
-	{
-
-
-		for(int j = 0; j < val; j++)
-		{
-			image[charsize*val*i+j].green = (255);
-			image[charsize*i+j*val].green = (255);
-		}
-	}
-	for(int f=0;f<n;f++)
-	{
-		for(int g=0;g<n;g++)
-		{
-			if(mat[f][g].getSymbol()=='X')
-			{
-				for(int loc=1;loc<charsize;loc++)
-				{
-					image[val*charsize*f+val*loc+g*charsize+loc].red = (255);
-					image[val*charsize*f+val*loc+g*charsize+loc].green = (0);
-					image[val*charsize*f+val*loc+g*charsize+loc].blue = (0);
-					image[val*charsize*f+val*loc+charsize+g*charsize-loc].red = (255);
-					image[val*charsize*f+val*loc+charsize+g*charsize-loc].green = (0);
-					image[val*charsize*f+val*loc+charsize+g*charsize-loc].blue = (0);
+		
+		
+		Board::Board(int len){
+			this->len = len;
+			
+			for (int i = 0; i < len; ++i) {
+				vector<BoardMem> tmp;
+				for (int j = 0; j < len; ++j) {
+					BoardMem c{i, j, '.'};
+					tmp.push_back(c);
 				}
-
+				board.push_back(tmp);
 			}
-			else if(mat[f][g].getSymbol()=='O')
-			{
-				for(int row=1;row<charsize;row++)
-				{
-					for(int loc=1;loc<charsize;loc++)
-					{
-						int rx=charsize/2;
-						int ry=charsize/2;
-						int dx = (rx- loc) ; // horizontal offset
-						int dy = (ry-row); // vertical offset
-						if ( (dx*dx + dy*dy) <= (rx*rx))
-						{
-							image[val*charsize*f+row*val+g*charsize+loc].red = (0);
-							image[val*charsize*f+row*val+g*charsize+loc].green = (0);
-							image[val*charsize*f+row*val+g*charsize+loc].blue = (128);
-						}
+			
+		}
+		Board::Board(const Board &b2) {
+			this->len = b2.len;
+			
+			for (int i = 0; i < len; ++i) {
+				vector<BoardMem> tmp;
+				for (int j = 0; j < len; ++j) {
+					BoardMem c(i, j, b2.board[i][j].val);
+					tmp.push_back(c);
+				}
+				board.push_back(tmp);
+			}
+		}
+		
+		
+		BoardMem & Board::operator[](BoardMem const &crdn) {
+			
+			int x1 = crdn.row;
+			int x2 = crdn.col;
+			
+			if(x1>=len || x2>=len || x1<0 || x2<0){
+				IllegalCoordinateException ie(x1,x2);
+				throw ie;
+			}
+			return this->board[x1][x2];
+		}
+		
+		
+		
+		
+		
+		
+				
+		ostream& operator<< (ostream& os, const Board& b){
+			for (int i = 0; i < b.len; ++i) {
+				for (int j = 0; j < b.len; ++j) {
+					os << b.board[i][j];
+				}
+				os<<endl;
+			}
+		return os ;
+		}
+		
+		
+		
+		Board& Board::operator= (char n){
+			
+			if(n!='.' && n!='O' && n!='X'){
+				IllegalCharException ie(n);
+				throw ie;
+			}
+			
+			for (int i = 0; i < len; ++i) {
+				for (int j = 0; j < len; ++j) {
+					board[i][j] = n;
+				}
+			}
+		return *this;
+		}
+		
+		
+		
+		Board& Board::operator= (Board b2){
+			this->len = b2.len;
+			vector<vector<BoardMem> > newBoard;
+			for (int i = 0; i < len; ++i) {
+				vector<BoardMem> tmp;
+				for (int j = 0; j < len; ++j) {
+					BoardMem c(i, j, b2.board[i][j].val);
+					tmp.push_back(c);
+				}
+				newBoard.push_back(tmp);
+			}
+			
+			board = newBoard;
+			return *this;
+		}
+		
+		
+		
+		istream &operator>>( istream  &input, Board &D ) { 
+			string s ;
+			string tmp;
+			
+			int count = 0;
+			while(!input.eof()) {
+				
+				count++;
+				s+=tmp;
+				input >>tmp;
+			}
+			
+			count--;
+			Board bb(count);
+			bb.init(s);
+			
+			D = bb;
+			
+			return input;            
+		}
+		
+		
+		void Board::init(string s){
+			int l = 0;
+			for(int i=0; i<this->len; i++){
+				for(int j=0; j<this->len; j++){
+					if(s[l]=='\n') l++;
+					if(l<s.size()){
+					this->board[i][j] = s[l];
+					l++;
 					}
 				}
 			}
 		}
+		
+		
+		bool Board::exist(const std::string& name){
+				
+				ifstream file(name);
+				if(!file)            // If the file was not found, then file is 0, i.e. !file=1 or true.
+					return false;    // The file was not found.
+				else                 // If the file was found, then file is non-0.
+					return true;     // The file was found.
+					
+			}
+
+		string Board::fileName (const std::string& name){
+			string s = name;
+			bool b = exist(s+".ppm");
+			if (b == false){
+				return s;
+			}
+			else return fileName(s + "1");
+		}
+		
+		
+		
+		
+		
+		string Board::draw(int dim){
+			string nf = fileName("board");//func("board");
+			nf+=".ppm";
+			paint(nf, dim);
+			return nf;
+		}
+		
+		
+	
+
+	//RGB *ima;
+	void Board::putpixel(int x, int y, int dim, RGB *ima){
+		(ima)[dim * (x)+(y)].red = 255;
+		(ima)[dim * (x)+(y)].green = 0;
+		(ima)[dim * (x)+(y)].blue = 0;
 	}
-	imageFile.write(reinterpret_cast<char*>(&image), 3*dimx*dimy);
-	imageFile.close();
-	return filename;
-}
+
+	void Board::drawcircle(int x0, int y0, int radius, int dim, RGB *ima){
+		int x = radius-1;
+		int y = 0;
+		int dx = 1;
+		int dy = 1;
+		int err = dx - (radius << 1);
+
+		while (x >= y)
+		{
+			putpixel(x0 + x, y0 + y, dim, ima);
+			putpixel(x0 + y, y0 + x, dim, ima);
+			putpixel(x0 - y, y0 + x, dim, ima);
+			putpixel(x0 - x, y0 + y, dim, ima);
+			putpixel(x0 - x, y0 - y, dim, ima);
+			putpixel(x0 - y, y0 - x, dim, ima);
+			putpixel(x0 + y, y0 - x, dim, ima);
+			putpixel(x0 + x, y0 - y, dim, ima);
+			
+			if (err <= 0)
+			{
+				y++;
+				err += dy;
+				dy += 2;
+			}
+			
+			if (err > 0)
+			{
+				x--;
+				dx += 2;
+				err += dx - (radius << 1);
+			}
+		}
+	}
+
+	void Board::drawx(int x0, int y0, int radius, int dim, RGB *ima){
+		radius *=2;
+		x0 = x0 - (radius/2);
+		y0 = y0 - (radius/2);
+		for(int i=0; i<=radius; i++){
+			putpixel(x0+i, y0+i, dim, ima);
+			putpixel(radius+x0-i, y0+i, dim, ima);
+		}	
+	}
+
+	//void paintCell(int row, int col, )
+
+	void Board::paint(string filename, int dim) {
+		
+		int boardSize = this->len;
+		int N = dim;
+		
+	  const int dimx = N, dimy = N;
+	  ofstream imageFile(filename, ios::out | ios::binary);
+	  imageFile << "P6" << endl << dimx <<" " << dimy << endl << 255 << endl;
+	 
+	 RGB image[dimx*dimy];
+	  for (int j = 0; j < dimy; ++j)  {  // row
+		for (int i = 0; i < dimx; ++i) { // column
+		  image[dimx*j+i].red = (255); //image[dimx*row + col]
+		  image[dimx*j+i].green = (255);
+		  image[dimx*j+i].blue = (255);
+		}
+	  }
+		int line = boardSize-1;
+		int cell (dimx / boardSize);
+		int mid = (dimx / boardSize )/ 2;
+		
+		for(int l =1; l<= line; l++){
+			int dot = ((dimx/boardSize)*l);
+			for (int j = 0; j < dimy; ++j)  {
+				
+				image[dimx * (dot)+(j)].red = 0;
+				image[dimx * (dot)+(j) ].green = 0;
+				image[dimx * (dot)+(j)].blue = 0;
+				image[dimx *(j)+(dot)].red = 0;
+				image[dimx *(j)+(dot)].green = 0;
+				image[dimx *(j)+(dot)].blue = 0;
+				
+		}
+	 }
+	 RGB *ima = image;
+	 for(int i=0; i<len; i++){
+		  for(int j=0; j<len; j++){
+			  if(board[i][j] == 'O'){
+				  drawcircle(cell*i + mid, cell*j + mid, mid - (mid/2), dimx, ima);
+			  }
+			  if(board[i][j] == 'X'){
+				  drawx(cell*i + mid, cell*j + mid, mid - (mid/2), dimx, ima);
+			  }
+		}
+	 }
+	 
+	  imageFile.write(reinterpret_cast<char*>(&image), 3*dimx*dimy);
+	  imageFile.close();
+	
+	}
